22372722ff36a1cd23b18bffb34f37e62a027d5d
Constantin Pape <c.pape@gmx.net>
2019-11-19 21:19:44 +0100

    Try to simplify cmake

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,11 +5,6 @@
 mkdir -p build
 cd build
 
-# Set the correct python flags, depending on the distribution
-PY_VER=$(python -c "import sys; print('{}.{}'.format(*sys.version_info[:2]))")
-PY_ABIFLAGS=$(python -c "import sys; print('' if sys.version_info.major == 2 else sys.abiflags)")
-PY_ABI=${PY_VER}${PY_ABIFLAGS}
-
 # we build with boost fs in macos
 if [[ "$OSTYPE" == "darwin"* ]]; then
     BOOST_FS=ON
@@ -24,7 +19,6 @@ cmake .. \
         -DCMAKE_C_COMPILER=${CC} \
         -DCMAKE_CXX_COMPILER=${CXX} \
         -DCMAKE_BUILD_TYPE=RELEASE \
-        -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}\
         -DCMAKE_INSTALL_PREFIX=${PREFIX} \
         -DCMAKE_PREFIX_PATH=${PREFIX} \
 \
@@ -42,11 +36,6 @@ cmake .. \
         -DWITH_LZ4=ON \
         -DWITHIN_TRAVIS=OFF \
         -DWITH_BOOST_FS=BOOST_FS \
-
-\
-        -DPYTHON_EXECUTABLE=${PYTHON} \
-        -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_ABI}${SHLIB_EXT} \
-        -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PY_ABI} \
 
 ##
 ## Compile and install
